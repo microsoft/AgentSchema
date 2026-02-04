8a80b17a11604afda90e5a4c06f3a1efacdcbf54
Seth Juarez <me@sethjuarez.com>
2026-02-04 15:44:13 -0800

    fix: relax eslint rules for emitter, add typescript-eslint dependency

--- agentschema-emitter/eslint.config.js
+++ agentschema-emitter/eslint.config.js
@@ -8,4 +8,14 @@ export default tsEslint.config(
   },
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "no-case-declarations": "off",
+      "no-prototype-builtins": "off",
+      "no-useless-escape": "off"
+    }
+  }
 );
